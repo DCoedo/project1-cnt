23c8cc221047dd7e7e163a42ffc8189adab5816f
DCoedo <98480357+DCoedo@users.noreply.github.com>
2022-01-30 16:54:37 -0500

    Update client.py

--- client.py
+++ client.py
@@ -8,6 +8,22 @@ ADDR = (HOST, PORT)
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(ADDR)
 
+#Command Line Tool
+program = sys.argv[0]
+host = sys.argv[1]
+port = sys.argv[2]
+fileName = sys.argv[3]
+
+
+@staticmethod
+def connectTCP(host: str, port: int, fileName: str):
+    try:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect((host, port))
+            return True
+    except:
+        return False
+
 
 
 
