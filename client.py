44af82fb2c9d30b539639908fa1b9ea6a532f8e8
DCoedo <98480357+DCoedo@users.noreply.github.com>
2022-01-30 20:23:18 -0500

    Update client.py

--- client.py
+++ client.py
@@ -1,31 +1,82 @@
 import socket
+import sys
+import argv
+from sys
 
 
 PORT = 65432
 HOST = socket.gethostbyname(socket.gethostname())
 ADDR = (HOST, PORT)
 
+def getMsg(string):
+    msg = string
+    bytes = b""
+    while bytes != msg:
+        bytes += sock.recv(2022)
+        if bytes == msg:
+            return False
+        if msg.find(bytes) != 0
+            bytes = b""
+
+def mail(fileName, sock):
+    try:
+        file = open(fileName, "rb")
+    except FileNotFoundError:
+        print(f"The file named {fileName} doesn't exit.")
+        quit()
+    else:
+        while True:
+            portion = file.read(10000)
+            if portion:
+                sock.send(portion)
+            else:
+                file.close()
+                break
+
+
+#Connects Client and Binds them to the address
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(ADDR)
 
-#Command Line Tool
-program = sys.argv[0]
-host = sys.argv[1]
-port = sys.argv[2]
-fileName = sys.argv[3]
+
+if (len(argv) != 4):
+    print("[ERROR]: Check command line parameters.")
+    sys.exit(1)
 
 
-@staticmethod
-def connectTCP(host: str, port: int, fileName: str):
-    try:
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.connect((host, port))
-            return True
-    except:
-        return False
+#Arguments needed
+projectName, HOST, PORT, fileName = argv
 
+if int(port) < 1 or int(port) > 65535:
+    print("[ERROR]: Invalid port")
+    quit(1)
 
+#10 second timeout counter
+sock.timeOut(10)
+sock
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, 2022)
+try:
+    sock.connect((HOST, int(PORT)))
 
+    getMsg(b"accio\r\n")# First Message
+
+    sock.send(b"confirm-accio\r\n")  # First Confirm
+
+    getMsg(b"accio\r\n")# Second Message
+
+    sock.send(b"confirm-accio-again\r\n")  # Second Confirm
+
+    sent = sock.send(b"\r\n")
+
+    mail(filename, sock)
+    sock.close()
+
+except socket.error: #TimeOut
+    #print("The connectixon took longer than 10 seconds")
+    sys.stderr.write("[ERROR]: Time-out error - connection took to long to be established.")
+    sys.exit(1)
+
+sys.exit(0)
 
 
 
